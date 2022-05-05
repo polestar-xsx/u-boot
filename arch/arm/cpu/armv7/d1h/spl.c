dc877f5999cb7879c270da4e6605689e7720f0f7
polestar_xsx <xusongxue88@163.com>
2022-05-06 00:03:43 +0800

    Add spl file

---
+++ arch/arm/cpu/armv7/d1h/spl.c
@@ -0,0 +1,16 @@
+#include <common.h>
+#include <asm/io.h>
+#include <asm/arch/base.h>
+#include <asm/arch/timer.h>
+#include <asm/arch/sfma.h>
+#include <arm/arch/sdram.h>
+#include <asm/arch/cpu.h>
+#include <asm/arch/clock.h>
+
+void board_init_f(ulong dummy)
+{
+    timer_init();
+    cpu_init();
+    clock_init();
+    sdram_init();
+}
